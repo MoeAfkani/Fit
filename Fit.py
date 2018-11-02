c3eba451b168aa55dfef455b65fe1210df55e3d4
Moonak <afkani.mohammad@gmail.com>
2018-11-02 09:27:26 +0330

    redeading data from text file solved

--- Fit.py
+++ Fit.py
@@ -7,6 +7,7 @@ An script to fit 3D data (x,y,y_err) on y =0.4 * (a + 1) *(b-x) - 0.434294 * 10.
 """
 
 # **********************************
+import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -23,16 +24,29 @@ def F(x,a,b,c):
     return 0.4 * (a + 1) *(b-x) - 0.434294 * 10.**(0.4 * (b-x) ) + c
 # The Fitting Function
 # **********************************
-# Valid Data
-xdata=[-23.75,-23.25,-22.75,-22.25,-21.75,-21.25,-20.75,-20.25,-19.75,-19.25,-18.75,-18.25,-17.75,-17.25]
-ydata=[-6.269055,-5.968025,-4.990302,-4.197173,-3.811173,-3.547245,-3.399823,-3.274298,-3.179857,-3.077325,-2.958362,-2.881843,-2.792513,-2.706881]
-yerr=[1.0,0.707107,0.229416,0.092057,0.059028,0.043561,0.036761,0.031814,0.028537,0.025359,0.022113,0.020249,0.01827,0.016554]
+# Read Valid Data
+f = open(sys.argv[1],"r")
+lines = f.readlines()
+f.close()
+f = open("newfile.txt","w")
+for line in lines:
+  if line!="#    x     y       error_y   "+"\n":
+    f.write(line)
+f.close()
 
-# Valid Data
+file = open('newfile.txt')
+lst = [] 
+for line in file:
+    lst.append([ float(x) for x in line.split()])
+xdata = [ x[0] for x in lst]
+ydata = [ x[1] for x in lst]
+yerr  = [ x[2] for x in lst]
+os.remove("newfile.txt") 
+# Read Valid Data
 # **********************************
 popt, pcov = curve_fit(F, xdata, ydata , sigma=yerr )
 popt
-xpints= np.linspace(min(xdata),max(xdata), 10000)
+xpints= np.linspace(min(xdata),max(xdata), 100000)
 plt.plot(xpints, F(xpints,*(popt)), 'r-', 
          label='Fit details:\n a=%5.5f$\pm$%.5e\n b=%5.5f$\pm$%.5e\n c=%5.5f$\pm$%.5e' %(popt[0],pcov[0][0],popt[1],pcov[0][1],popt[2],pcov[0][2]))
 plt.errorbar(xdata, ydata, yerr=yerr,fmt='.k', color='black',
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Fit
 Fit a function to a data set
+
+# How to run
+
+./Fit.py [data.text]
+
+
+# Tip
+
+Fit.py must be excutable
